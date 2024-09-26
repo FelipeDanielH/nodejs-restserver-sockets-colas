6e649cd2f69309350edcd0785da02ad87efaf925
Felipe <felipe.daniel.henriquez@gmail.com>
2024-09-26 15:53:45 -0300

    central;iza logica de tickets pt2

--- models/ticket-control.js
+++ models/ticket-control.js
@@ -1,8 +1,10 @@
+const path = require('path');
+const fs = require('fs');
 
-/* Esto va a sr una clase comun y corriente */
+/* Esto va a ser una clase comun y corriente */
 class TicketControl {
     // En el constructor vamos a poner todas las clases que vamos a ocupar
-    contructor() {
+    constructor() {
         // propiedad para almacenar el ultimo ticket que se esta atendiendo
         this.ultimo = 0;
 
@@ -30,13 +32,21 @@ class TicketControl {
 
     // metodo para inicializar la clase (servidor)
     init() {
-        const data = require('../db/data.json'); // con esta linea podemos convertir todo el archivo json a objeto literal de javascript donde podremos consultar tanto datos como propiedads tal cual un objeto normal 
-        console.log(data);
-        console.log("uwu");
+        const {hoy, tickets, ultimo, ultimos4 } = require('../db/data.json'); // con esta linea podemos convertir todo el archivo json a objeto literal de javascript donde podremos consultar tanto datos como propiedads tal cual un objeto normal 
+        if ( hoy === this.hoy ) { // si la data del dia que esta en la base de datos coincide con la data que esta guardada en la variable de esta clase (correspondiente al dia de hoy) entonces se guardan todos los datos de la bd en las variables de esta clase
+            this.tickets = tickets;
+            this.ultimo = ultimo;
+            this.ultimos4 = ultimos4;
+        }else {
+            // si no coinciden los datos significa que es otro dia y hay que reinicializar las variables
+            this.guardarDB();
+        }
     }
 
     guardarDB () {
+        const dbPath = path.join( __dirname, '../db/data.json') // __dirname apunta al directorio de este archivo, que se encuentra dentro de la carpeta 'modelos', por lo que hay que ir una atras y por eso el ../db
 
+        fs.writeFileSync( dbPath, JSON.stringify( this.toJson ) )
     }
 }
 
