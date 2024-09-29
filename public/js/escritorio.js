9be3e7470279176bcbd429afb4e93b1799de7d1e
Felipe <felipe.daniel.henriquez@gmail.com>
2024-09-28 21:51:40 -0300

    se prepara pantalla de escritorio

--- public/js/escritorio.js
+++ public/js/escritorio.js
@@ -1,3 +1,38 @@
+// La siguiente linea solo funciona en chrome y en firefox
+const searchParams = new URLSearchParams( window.location.search );
+
+// Referencias HTML
+const lblEscritorio = document.querySelector('h1');
+btnAtender = document.querySelector('button');
 
 
-console.log('Escritorio HTML');
+const socket = io();
+
+if ( !searchParams.has('escritorio')){
+    window.location = 'index.html'
+    throw new Error('El escritorio es obligatorio');
+}
+
+const escritorio = searchParams.get('escritorio');
+lblEscritorio.innerText = escritorio;
+
+socket.on('connect', () => {
+    btnAtender.disabled = false;
+});
+
+socket.on('disconnect', () => {
+    btnAtender.disabled = true;
+});
+
+socket.on('ultimo-ticket',(ultimoTicket) => {
+    // lblNuevoTicket.innerText = 'Ticket: ' + ultimoTicket;
+})
+
+btnAtender.addEventListener( 'click', () => {
+
+    // La definicion del listener 'siguiente ticket' esta en '../sockets/controller.js'
+    // socket.emit( 'siguiente-ticket', null, ( ticket ) => {
+    //     lblNuevoTicket.innerText = ticket;
+    // });
+
+});
